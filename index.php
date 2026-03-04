762af452211a6f8fd0112b455c51d30b899f70a9
Isa Araújo <isabelaa.ifpr2024@gmail.com>
2026-03-04 09:55:26 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+echo '<link rel="stylesheet" href="style.css">';
+
+$gremio = new Link("https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Gremio_logo.svg/250px-Gremio_logo.svg.png");
+$inter = new Link("https://a.espncdn.com/i/teamlogos/soccer/500/1936.png");
+$chape = new Link("https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Logo_Associa%C3%A7%C3%A3o_Chapecoense_de_Futebol.svg/1280px-Logo_Associa%C3%A7%C3%A3o_Chapecoense_de_Futebol.svg.png");
+
+$times = [$gremio, $inter, $chape];
+
+
+$acdc = new Link("https://imusic.b-cdn.net/images/item/original/432/5050293858432.jpg?ac-dc-ac-dc-logo-slipmat-vinyl-accessory&class=scaled&v=1638973670");
+$guns = new Link("https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2020/07/Guns-N-Roses-Appetite-For-Destruction.jpg");
+$rhcp = new Link("https://m.media-amazon.com/images/M/MV5BYjc0Njk5NjUtMzk5Yy00MmUzLThmODEtYzA1YjZhNWU0ZDYxXkEyXkFqcGc@._V1_.jpg");
+
+$bandas = [$acdc, $guns, $rhcp];
+
+ 
+$brasil = new Link("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Flag_of_Russia.svg/960px-Flag_of_Russia.svg.png");
+$russia = new Link("https://static.todamateria.com.br/upload/ba/nd/bandeira-do-brasil-og.jpg");
+$canada = new Link("https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Canada_%28Pantone%29.svg");
+
+$paises = [$brasil, $russia, $canada];
+
+
+$ditto = new Link("https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/132.png");
+$gengar = new Link("https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/094.png");
+$mewtwo = new Link("https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/150.png");
+
+$pokemons = [$ditto, $gengar, $mewtwo];
+      
+
